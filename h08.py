cec390798bbba119f34878a89bb6037aef9d203a
jaanus55 <jaanus.salm@gmail.com>
2025-01-17 10:45:27 +0200

    H8.1 done

---
+++ h08.py
@@ -0,0 +1,38 @@
+h08.py
+
+telefonid={
+'Mari': '5957503',
+'Toomas': '5719979',
+'Kertu': '5201750',
+'Siim': '5580027',
+'Piret': '5960799',
+'Jaan': '5160415',
+'Lea': '5760164',
+'Mart': '5337951',
+'Anni': '5004818',
+'Tõnu': '5721873',
+'Kai': '5811884',
+'Rasmus': '5859489',
+'Eva': '5039393',
+'Oskar': '5635812',
+'Liina': '5696114',
+'Peeter': '5560756',
+'Sandra': '5257415',
+'Heiki': '5207248',
+'Kristi': '5703338',
+'Urmas': '5400549'
+}
+print(f"Rasmuse telnr on: {telefonid["Rasmus"]}")
+telefonid.pop("Kristi")
+telefonid["Eva"]="555555555"
+print(telefonid)
+print("------------")
+for nimi in telefonid:
+    print(telefonid[nimi], end= "")
+
+print()
+try:
+    otsi = input("otsi": "")
+    print(f"leitud: {telefonid[otsi]}")
+except;
+    print("Otsitavat ei leitud")
