b16bb35a69aafbd34e3b8995e8deb568c21fbd75
jaanus55 <jaanus.salm@gmail.com>
2025-01-17 11:50:37 +0200

    h9tiktak

---
+++ H09.py
@@ -0,0 +1,11 @@
+H09.py
+#Kuva arvud 1-42
+
+for i in range(1.43):
+    if i%3==0:
+        print(f"{1} TIK")
+    elif i%5 ==0:
+        print(f"{i} TAK")
+    elif i%3==0 and i%5==0:
+        print(f"{i} TIKTAK")
+        elif i%3==0
