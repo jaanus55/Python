e9c507d9a1f33fa22f9d3f3f5a1a4a0ea0513f45
jaanus55 <jaanus.salm@gmail.com>
2025-02-14 09:43:34 +0200

    yl19

--- yl17.py
+++ yl17.py
@@ -1,4 +1,20 @@
 with open("pangakonto.txt") as fail:
     sisu = fail. readlines()
 for number in sisu:
-    print(float(number))
+    #print(float(number))
+    tehingute_arv+=1
+    if float(number) > 0:
+        tehingute_arv_pos += 1
+        pos_arv_summa += float(number)
+
+
+        print(f"Tehingute arv: {tehingute_arv}")
+        print(f"Positiivsete tehingute arv: {tehingute_arv_pos}")
+        print(f"Positiivsete arvude summa: {pos_arv_summa: 2f)")
+        
+
+
+npalgad = 0
+#See on teine ülesanne
+with open("palgad.txt") as fail:
+    sisu = fail. readlines
